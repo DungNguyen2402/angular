ed8f071f8fbae31e6a23e24e7802105fc830e600
DungNguyen2402 <117297846+DungNguyen2402@users.noreply.github.com>
2023-05-28 11:04:58 +0700

    done : update ,detail

--- src/app/pages/admin/product-edit/product-edit.component.ts
+++ src/app/pages/admin/product-edit/product-edit.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from '../../interfaces/Product';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.scss']
+  styleUrls: ['./product-edit.component.scss'],
 })
 export class ProductEditComponent {
+  product: any = {
+    name: '',
+    price: '',
+  };
 
+  productForm = this.formBuilder.group({
+    name: [''],
+    price: [0],
+  });
+
+  constructor(
+    private productService: ProductService,
+    private router: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {
+    this.router.paramMap.subscribe((param) => {
+      const id = Number(param.get('id'));
+      this.productService.getProductById(id).subscribe((product) => {
+        this.product = product;
+        this.productForm.patchValue({
+          name: product.name,
+          price: product.price,
+        });
+      });
+    });
+  }
+  onHandleSubmit() {
+    if (this.productForm.invalid) return;
+
+    const product: IProduct = {
+      id: this.product.id,
+      name: this.productForm.value.name || '',
+      price: this.productForm.value.price || 0,
+    };
+    this.productService.updateProduct(product).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent {
-
+  products: any = {
+    name: '',
+    price: 0,
+  };
+  constructor(private product: ProductService, private route: ActivatedRoute) {
+    this.route.paramMap.subscribe((param) => {
+      const id = Number(param.get('id'));
+      this.product.getProductById(id).subscribe((data) => {
+        this.products = data;
+      });
+    });
+  }
 }
